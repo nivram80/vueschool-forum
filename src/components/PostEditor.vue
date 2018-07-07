b88adfcb94e732417646127e2234a40f6000952c
Todd Nichols <nivram80@gmail.com>
2018-07-07 11:38:46 -0500

    initial commit

---
+++ src/components/PostEditor.vue
@@ -0,0 +1,44 @@
+<template>
+  <form @submit.prevent="save">
+    <div class="form-group">
+      <textarea
+        class="form-input"
+        cols="30"
+        rows="10"
+        v-model="text"
+      ></textarea>
+    </div>
+
+  </form>
+</template>
+<script>
+  export default {
+    name: 'PostEditor',
+    props: {
+      threadId: {
+        required: true,
+        type: String
+      }
+    },
+    data () {
+      return {
+        text: ''
+      }
+    },
+    methods: {
+      save () {
+        const postId = 'greatPost' + Math.random()
+        const post = {
+          text: this.text,
+          publishedAt: Math.floor(Date.now() / 1000),
+          threadId: this.threadId,
+          userId: '7uVPJS9GHoftN58Z2MXCYDqmNAh2',
+          '.key': postId
+        }
+
+        this.text = ''
+        this.$emit('save', { post })
+      }
+    }
+  }
+</script>
---
+++ src/router/index.js
@@ -0,0 +1,29 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from '@/components/pages/PageHome'
+import ThreadShow from '@/components/pages/PageThreadShow'
+import NotFound from '@/components/pages/PageNotFound'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/thread/:id',
+      name: 'ThreadShow',
+      component: ThreadShow,
+      props: true
+    },
+    {
+      path: '*',
+      name: 'NotFound',
+      component: NotFound
+    }
+  ],
+  mode: 'history'
+})
